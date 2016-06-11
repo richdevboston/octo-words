2b8211cbc2f77a0d8124f3016e850e717a45f45b
Yassine Maaroufi <yassinemaaroufi@gmail.com>
2016-06-11 11:47:16 +0000

    Words resize according to their frequency

--- README.md
+++ README.md
@@ -12,11 +12,7 @@ This application is built on top of Google App Engine and Tornado.
 
 # TODO
 + Handle URLs: Add http:// if necessary
-+ Retrieve the page data
-+ Use regex to fetch body content
-+ Remove HTML tags
 + Remove prepositions, keep only verbs and nouns
-+ Count the number of occurences
 + Sort by frequency
 + Display as word cloud
 + Enter into DB
--- web/main.py
+++ web/main.py
@@ -29,12 +29,41 @@ class URLHandler(tornado.web.RequestHandler):
         url = self.get_argument("url", default=None, strip=False)
         response = 'Nothing yet'
 
-        response = wget(url).replace('\r\n','').replace('\n', '')
-        regex = re.compile('(?<=body).*?(?=<\/body>)')
-        response = regex.findall(response)[0]
-        response = re.sub(r'<.*?>', ' ', response)
+        # Fetch URL and extract content
+        response = wget(url).replace('\r\n','').replace('\n', '')   # Fetch URL and remove newlines
+        regex = re.compile('(?<=body).*?(?=<\/body>)')              # extract content of body HTML tag
+        response = regex.findall(response)[0]                       
+        response = re.sub(r'<.*?>', ' ', response)                  # Remove HTML tags
+
+        # Remove prepositions and articles
+        prep_arr = [
+                'on', 'in', 'at', 'since', 'for', 'ago', 'before', 'to', 'until', 'till', 'by',
+                'off', 'about', 'from', 'onto', 'unto', 'into', 'through', 'across', 
+                'above', 'below', 'over', 'under', 'beside', 'next',
+                'a', 'an', 'the', 'some', 'few', 'this', 'that', 'those', 'these'
+                'how', 'why', 'what', 'who', 'when', 'there', ]
+        for i in prep_arr:
+            response = response.replace(i, '')
+
+        # Create word dictionary and frequency
+        wordcount = {}
+        regex = re.compile('[a-zA-Z\-]{3,}')
+        words = regex.findall(response)
+        for word in words:
+            if word not in wordcount:
+                wordcount[word] = {'freq': 1, 'fontSize': 1}
+            else:
+                wordcount[word]['freq'] += 1
+
+        # Sort words by frequency
+        high = 0
+        for word in wordcount:
+            high = max(high, wordcount[word]['freq'])
+        
+        for word in wordcount:
+            wordcount[word]['fontSize'] = float(wordcount[word]['freq']) / float(high) * float(400)
             
-        self.render("word.html", url=url, results=response)
+        self.render("word.html", url=url, results=wordcount)
 
 def handle_request(response):
     '''Callback needed when a response arrives.'''
